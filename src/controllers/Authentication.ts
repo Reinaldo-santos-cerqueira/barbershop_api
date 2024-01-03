4cf211ef10a389b3a97fc926818321853b503d73
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-03 17:58:09 -0300

    feat: new function to AuthenticationControllers responsibile for created client and employee

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -1,7 +1,31 @@
+import { CreateClient, CreateEmployee } from '@service';
 import { Request, Response } from 'express';
 
 export class AuthenticationControllers {
-    login(request: Request, response: Response) {
-        return response.json({ message: 'teste' });
+    async createdClient(request: Request, response: Response): Promise<Response> {
+        try {
+            const { client, address } = request.body;
+            const createClient = new CreateClient();
+            const clientNew = await createClient.execute({ client, address });
+            console.log(clientNew);
+            return response.json({ message: 'Criado com sucesso' }).status(200);
+        } catch (error) {
+            console.log(error);
+
+            return response.json({ message: 'Internal server error' });
+        }
+    }
+    async createdEmployee(request: Request, response: Response): Promise<Response> {
+        try {
+            const { employee } = request.body;
+            const createEmployee = new CreateEmployee();
+            const employeeNew = await createEmployee.execute({ employee });
+            console.log(employeeNew);
+            return response.json({ message: 'Criado com sucesso' }).status(200);
+        } catch (error) {
+            console.log(error);
+
+            return response.json({ message: 'Internal server error' });
+        }
     }
 }
