d817453d2cbaa815ae01aec19ce487fb18473956
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-03 18:52:10 -0300

    feat: created function to validate password and encrypted

---
+++ src/utils/encryptValidatePassword.ts
@@ -0,0 +1,16 @@
+import { AppError } from '@middlewares';
+import bcrypt from 'bcrypt';
+export async function encryptValidatePassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    const passwordStrongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-_=+{};:'",.<>?/\\|]).{10,}$/;
+
+    try {
+        if (!passwordStrongRegex.test(password)) {
+            throw new AppError('Senha fraca precisa ter: 10 caracteres,pelo menos uma letra minúscula,pelo menos uma letra maiúscula,pelo menos um número e pelo menos um caractere especial', 400);
+        }
+        const hash = await bcrypt.hash(password, saltRounds);
+        return hash;
+    } catch {
+        throw new AppError('Erro ao encryptar a senha. Tente novamente.', 500);
+    }
+}
