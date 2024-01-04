7c4b00f1e50a078ae4e49e72d3165f9927b1e203
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-04 12:29:55 -0300

    refactor: remove if in to out

--- src/utils/encryptValidatePassword.ts
+++ src/utils/encryptValidatePassword.ts
@@ -3,14 +3,16 @@ import bcrypt from 'bcrypt';
 export async function encryptValidatePassword(password: string): Promise<string> {
     const saltRounds = 10;
     const passwordStrongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-_=+{};:'",.<>?/\\|]).{10,}$/;
-
+    if (!passwordStrongRegex.test(password)) {
+        throw new AppError('Senha fraca precisa ter: 10 caracteres,pelo menos uma letra minúscula,pelo menos uma letra maiúscula,pelo menos um número e pelo menos um caractere especial', 400);
+    }
     try {
-        if (!passwordStrongRegex.test(password)) {
-            throw new AppError('Senha fraca precisa ter: 10 caracteres,pelo menos uma letra minúscula,pelo menos uma letra maiúscula,pelo menos um número e pelo menos um caractere especial', 400);
-        }
+
         const hash = await bcrypt.hash(password, saltRounds);
         return hash;
-    } catch {
+    }
+    catch {
         throw new AppError('Erro ao encryptar a senha. Tente novamente.', 500);
     }
 }
+
