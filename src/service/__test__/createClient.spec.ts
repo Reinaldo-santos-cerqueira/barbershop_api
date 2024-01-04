a7f65f95b7edd0bc80f93bbc760620ca4ea82104
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-04 16:41:10 -0300

    test: created test to verify if CreateClient to have be called

---
+++ src/service/__test__/createClient.spec.ts
@@ -0,0 +1,20 @@
+import { CreateClient } from '../'; // Substitua com o caminho correto
+import { ClientRepositories } from '@repositories';
+import { mockUserAddress } from './mock/userMock';
+import { Prisma } from '@prisma/client';
+beforeEach(() => {
+    jest.clearAllMocks();
+
+});
+describe('CreateClient', () => {
+    it('Should create a new client', async () => {
+        const createClient = new CreateClient();
+        const mockAddress = { ...mockUserAddress.address } as Prisma.addressCreateInput;
+        const mockUser = { ...mockUserAddress.client } as Prisma.clientCreateInput;
+
+        const createSpy = jest.spyOn(ClientRepositories.prototype, 'create');
+        createSpy.mockResolvedValue();
+        await createClient.execute({ address: mockAddress, client: mockUser });
+        expect(createSpy).toHaveBeenCalled();
+    });
+});
---
+++ src/service/__test__/mock/userMock.ts
@@ -0,0 +1,21 @@
+export const mockUserAddress = {
+    'address': {
+        'city': 'Salvador',
+        'cep': '41205235',
+        'number': '5',
+        'country': 'Brazil',
+        'uf': 'BA'
+    },
+    'client': {
+        'user': 'admin1',
+        'name': 'Reinaldo',
+        'email': 'reinaldodevcontato@gmail.com',
+        'phone': '71988853971',
+        'password': 'Reinaldo@123',
+        'photo': 'https://www.bing.com/images/search?view=detailV2&ccid=ArEOIyD%2b&id=43AABC2125CFDE6EAD7EDC628F08A9509709270C&thid=OIP.ArEOIyD-dIi64VqBztCPMAHaFA&mediaurl=https%3a%2f%2fcdn.pixabay.com%2fphoto%2f2017%2f06%2f30%2f11%2f17%2ftree-2458047_960_720.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.02b10e2320fe7488bae15a81ced08f30%3frik%3dDCcJl1CpCI9i3A%26pid%3dImgRaw%26r%3d0&exph=650&expw=960&q=pixabay&simid=608009340727922994&FORM=IRPRST&ck=665405569630D958934BD1CEC6C74AC3&selectedIndex=0&itb=1',
+        'status': 'ACTIVE',
+        'dateOfBirth': '1997-11-05T16:39:57-08:00',
+        'gender': 'MALE',
+        'document': '15342392004'
+    }
+};
