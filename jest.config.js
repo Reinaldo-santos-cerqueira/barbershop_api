397594efe52290f8a1c4848aec53a53c3b9a2382
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-04 12:25:47 -0300

    chore: configuration paths in jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    rootDir: __dirname,
+    moduleNameMapper: {
+        '@routes': '<rootDir>/src/routes',
+        '@controllers': '<rootDir>/src/controllers',
+        '@middlewares': '<rootDir>/src/middlewares',
+        '@utils': '<rootDir>/src/utils',
+        '@service': '<rootDir>/src/service',
+        '@models': '<rootDir>/src/models',
+        '@repositories': '<rootDir>/src/repositories'
+    }
 };
