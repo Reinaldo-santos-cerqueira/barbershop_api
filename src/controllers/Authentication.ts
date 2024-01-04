e0ac050944f6379eef19fdace401a617b70c353c
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-04 12:26:50 -0300

    refactor: remove try catch for controllers

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -1,31 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CreateClient, CreateEmployee } from '@service';
 import { Request, Response } from 'express';
-
 export class AuthenticationControllers {
     async createdClient(request: Request, response: Response): Promise<Response> {
-        try {
-            const { client, address } = request.body;
-            const createClient = new CreateClient();
-            const clientNew = await createClient.execute({ client, address });
-            console.log(clientNew);
-            return response.json({ message: 'Criado com sucesso' }).status(200);
-        } catch (error) {
-            console.log(error);
-
-            return response.json({ message: 'Internal server error' });
-        }
+        const { client, address } = request.body;
+        const createClient = new CreateClient();
+        await createClient.execute({ client, address });
+        return response.json({ message: 'Criado com sucesso' }).status(200);
     }
     async createdEmployee(request: Request, response: Response): Promise<Response> {
-        try {
-            const { employee } = request.body;
-            const createEmployee = new CreateEmployee();
-            const employeeNew = await createEmployee.execute({ employee });
-            console.log(employeeNew);
-            return response.json({ message: 'Criado com sucesso' }).status(200);
-        } catch (error) {
-            console.log(error);
-
-            return response.json({ message: 'Internal server error' });
-        }
+        const { employee } = request.body;
+        const createEmployee = new CreateEmployee();
+        await createEmployee.execute({ employee });
+        return response.json({ message: 'Criado com sucesso' }).status(200);
     }
 }
