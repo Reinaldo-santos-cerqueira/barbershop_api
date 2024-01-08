52bb05228cc3dfaf3208d0ecc18464424f04e645
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-08 17:40:51 -0300

    feat: added try catch and news erros throws AppError

--- src/service/createEmployee/createEmployee.ts
+++ src/service/createEmployee/createEmployee.ts
@@ -1,3 +1,4 @@
+import { AppError } from '@errors';
 import { Prisma } from '@prisma/client';
 import { EmployeeRepositories } from '@repositories';
 import { encryptValidatePassword } from '@utils';
@@ -9,8 +10,15 @@ interface ICreateEmployee {
 export class CreateEmployee {
     async execute({ employee }: ICreateEmployee) {
         const employeeRepositories = new EmployeeRepositories();
+        if (await employeeRepositories.verifyExists(employee.user)) {
+            throw new AppError('Já possui um usuario desse no banco', 400);
+        }
         const passwordEncrypted = await encryptValidatePassword(employee.password);
-        const newEmployee = await employeeRepositories.create({ ...employee, password: passwordEncrypted });
-        return newEmployee;
+        try {
+            const newEmployee = await employeeRepositories.create({ ...employee, password: passwordEncrypted });
+            return newEmployee;
+        } catch (e) {
+            throw new AppError('Internal server error', 500);
+        }
     }
 }
