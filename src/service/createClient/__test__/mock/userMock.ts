2a44d4bea3c4c914c2a17a4cd5bec89d8cf83005
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-08 15:28:19 -0300

    feat: add more props mock user

--- src/service/createClient/__test__/mock/userMock.ts
+++ src/service/createClient/__test__/mock/userMock.ts
@@ -7,6 +7,8 @@ export const mockUserAddress = {
         'uf': 'BA'
     },
     'client': {
+        id: '1',
+
         'user': 'admin1',
         'name': 'Reinaldo',
         'email': 'reinaldodevcontato@gmail.com',
@@ -14,8 +16,10 @@ export const mockUserAddress = {
         'password': 'Reinaldo@123',
         'photo': 'https://www.bing.com/images/search?view=detailV2&ccid=ArEOIyD%2b&id=43AABC2125CFDE6EAD7EDC628F08A9509709270C&thid=OIP.ArEOIyD-dIi64VqBztCPMAHaFA&mediaurl=https%3a%2f%2fcdn.pixabay.com%2fphoto%2f2017%2f06%2f30%2f11%2f17%2ftree-2458047_960_720.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.02b10e2320fe7488bae15a81ced08f30%3frik%3dDCcJl1CpCI9i3A%26pid%3dImgRaw%26r%3d0&exph=650&expw=960&q=pixabay&simid=608009340727922994&FORM=IRPRST&ck=665405569630D958934BD1CEC6C74AC3&selectedIndex=0&itb=1',
         'status': 'ACTIVE',
-        'dateOfBirth': '1997-11-05T16:39:57-08:00',
+        'dateOfBirth': new Date('1997-11-05T16:39:57-08:00'),
         'gender': 'MALE',
-        'document': '15342392004'
+        'document': '15342392004',
+        createdAt: new Date(),
+        updatedAt: new Date(),
     }
 };
